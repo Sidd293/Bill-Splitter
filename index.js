d6967249a1b2fe181416e5d07debaf5b2c931989
siddhartha <bajpai.siddhartha.stp@gmail.com>
2021-10-06 22:12:25 +0530

    commited

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ let ct  =0;
 users =[];
 d.addEventListener("click",()=>{
   amount  = prompt("what is the amount");
+ amount =  amount?amount:0;
     d.innerText = amount+"Rs";
-users.forEach((each,count)=>document.getElementById(count).innerHTML = `<div class = "card" ><div class = "userN">${each.username}</div> <div class = "amt">${amount/(ct)}</div></div>`)
+users.forEach((each,count)=>document.getElementById(count).innerHTML = `<div class = "card" id = ${count} ><div class = "userN">${each.username}</div> <div class = "amt">${amount/(ct)}</div></div>`)
 
     
 })
@@ -14,14 +15,26 @@ users.forEach((each,count)=>document.getElementById(count).innerHTML = `<div cla
 document.getElementById("new_user").addEventListener(
     "click",()=>{
     let user  = prompt("what is the name of th user")
+    if(user){
     nUser  = document.createElement("div")
     nUser.id = ct;
-   nUser.innerText = user +" "+ amount;
+//    nUser.innerText = user +" "+ amount;
    users.push({username:user,bill :"0",id :ct,})
 
     document.getElementById("users").appendChild(nUser);
-users.forEach((each,count)=>document.getElementById(count).innerHTML = `<div class = "card" ><div class = "userN">${each.username}</div> <div class = "amt">${amount/(ct+1)}</div></div>`)
+users.forEach((each,count)=>document.getElementById(count).innerHTML = `<div class = "card" id = ${count} ><div class = "userN">${each.username}</div> <div class = "amt">${amount/(ct+1)}</div></div>`)
 ct++;
+    }
 }
 )
+document.getElementById("backspace").addEventListener(
+    "click",()=>{
 
+       if(ct>0)
+        {users.pop();
+           document.getElementById(ct-1).innerHTML= "";
+ct--;}
+
+users.forEach((each,count)=>document.getElementById(count).innerHTML = `<div class = "card" id = ${count} ><div class = "userN">${each.username}</div> <div class = "amt">${amount/(ct)}</div></div>`)
+
+    })
